f082f10d93120a37e4ffd60295ea8ff89cad4287
alexadrien <alex.adrien.auger@gmail.com>
2019-06-05 15:58:26 +0200

    Save

--- commands/help_command.js
+++ commands/help_command.js
@@ -2,17 +2,23 @@ module.exports = (async () => {
     console.log(`
     
     Thanks for using Guevara. Here is some help on usage : 
-    
+        
+        🚧
         guevara init
     
     Type this command in the root folder of your projet and follow instructions. You will generate and fill some information to make Guevara works.
     
+    
+        💻
         guevara dev
     
     Type this command when you want to start a new feature. This will ask you to select a User Story and prepare your computer to develop this feature.
     
+    
+        🙏
         guevara pr
     
-    type this command when you a ready to create a Merge / Pull Request ready for review. This will push for you and create the PR/MR  
+    type this command when you a ready to create a Merge / Pull Request ready for review. This will push for you and create the PR/MR
+      
     `);
 });
