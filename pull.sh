26b2305f6957f37a4faedfb2dda76ca0da460172
alexadrien <alex.adrien.auger@gmail.com>
2019-07-23 10:03:25 +0200

    Add script to analyze analytics

---
+++ pull.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+#rm -rf segment-logs
+#aws s3 sync s3://guevara-analytics .
+#find . -name "*.gz" -exec gunzip {} \;
+#find . -name "*" -type f -exec python -m json.tool {} \; > output
