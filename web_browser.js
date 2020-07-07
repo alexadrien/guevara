f9b55214b00be6472126799ee916413d8d47842e
Nicolas Tarzia <git@nicolastarzia.com>
2020-07-07 09:57:55 -0300

    :package: Update package

--- web_browser.js
+++ web_browser.js
@@ -1,18 +1,18 @@
-const opn = require('opn');
+const opn = require("open");
 const { USER_DATA_KEYS } = require("./user_data");
 const { getEnvValue } = require("./user_data");
 
-const openInNewTab = (async url => {
+const openInNewTab = async (url) => {
   await opn(url, { wait: false });
-});
+};
 
-const openPullRequestInNewTab = (async pullRequest => {
+const openPullRequestInNewTab = async (pullRequest) => {
   if (!!(await getEnvValue(USER_DATA_KEYS.GITLAB_API_TOKEN))) {
     await openInNewTab(pullRequest.web_url);
   } else if (!!(await getEnvValue(USER_DATA_KEYS.GITHUB_ACCESS_TOKEN))) {
     await openInNewTab(pullRequest.html_url);
   }
-});
+};
 
 module.exports = {
   openPullRequestInNewTab,
