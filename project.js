ed038ff1e2bc339fe7db11eca0784ff8a9d81096
Nicolas Tarzia <git@nicolastarzia.com>
2020-07-07 11:34:24 -0300

    Return to default branch after PR

--- project.js
+++ project.js
@@ -1,10 +1,11 @@
-const child_process = require('child-process-promise');
+const { USER_DATA_KEYS } = require("./user_data");
+const { getEnvValue } = require("./user_data");
+const child_process = require("child-process-promise");
 
-const getBranchNameFromTicket = ticket => ticket.url
-  .replace(ticket.shortUrl, '')
-  .split('-')[0];
+const getBranchNameFromTicket = (ticket) =>
+  ticket.url.replace(ticket.shortUrl, "").split("-")[0];
 
-const prepareProjectForTheNewFeature = async ticket => {
+const prepareProjectForTheNewFeature = async (ticket) => {
   const branchToCreate = `feature${getBranchNameFromTicket(ticket)}`;
   const command = `git checkout -B ${branchToCreate}`;
   console.log(`
@@ -13,12 +14,24 @@ const prepareProjectForTheNewFeature = async ticket => {
   const result = await child_process.exec(command);
 };
 
+const checkoutToDefaultBranch = async () => {
+  const defaultBranchName = await getEnvValue(USER_DATA_KEYS.MAIN_BRANCH);
+  const command = `git checkout ${defaultBranchName}`;
+  console.log(`
+  Running : ${command}
+  `);
+  const result = await child_process.exec(command);
+};
+
 const getProjectActiveBranch = async () => {
-  const activeBranchCommand = await child_process.exec(`git rev-parse --abbrev-ref HEAD`);
+  const activeBranchCommand = await child_process.exec(
+    `git rev-parse --abbrev-ref HEAD`
+  );
   return activeBranchCommand.stdout.replace(/\n/g, "");
 };
 
 module.exports = {
   prepareProjectForTheNewFeature,
   getProjectActiveBranch,
+  checkoutToDefaultBranch,
 };
